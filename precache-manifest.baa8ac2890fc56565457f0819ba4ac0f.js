04960e01b5f0dcb890456d1dd6a7af10af20e236
marie1881 <53557430+marie1881@users.noreply.github.com>
2020-02-18 19:21:50 +0800

    Updates

---
+++ precache-manifest.baa8ac2890fc56565457f0819ba4ac0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f415eb9e3bdd2bab3b2c3656f57d4374",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "275e40bef2d00082c6cf",
+    "url": "/clicky-game/static/css/main.b0eda54b.chunk.css"
+  },
+  {
+    "revision": "646a75c58be7259d17ba",
+    "url": "/clicky-game/static/js/2.8c5b1c2e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/clicky-game/static/js/2.8c5b1c2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "275e40bef2d00082c6cf",
+    "url": "/clicky-game/static/js/main.15e37eeb.chunk.js"
+  },
+  {
+    "revision": "a444cfe878aa3a515eec",
+    "url": "/clicky-game/static/js/runtime-main.24c47d54.js"
+  }
+]);
